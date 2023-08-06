72e33e2bb697e21992aff85a7405d765a7021b79
jos-gabriel <jos.gabrie@gmail.com>
2023-08-05 18:27:05 -0600

    feat(model): endpoint Villa Dto clase villaStore con datos ficticios

---
+++ MaVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,17 @@
+﻿using MaVilla_API.Datos;
+using MaVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+    }
+}
---
+++ MaVilla_API/Modelos/Dto/VillaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MaVilla_API.Modelos.Dto
+{
+    public class VillaDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
