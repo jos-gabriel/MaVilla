814f1d92f076d826c96021a6a75fdffdd102fd1d
jos-gabriel <jos.gabrie@gmail.com>
2023-08-07 21:17:53 -0600

    refactor(Modelo-villa): Se añaden propiedades

--- MaVilla_API/Modelos/Dto/VillaDto.cs
+++ MaVilla_API/Modelos/Dto/VillaDto.cs
@@ -10,7 +10,13 @@ namespace MaVilla_API.Modelos.Dto
         [MaxLength(30)]
         public string Nombre { get; set; }
 
+        public string Detalle { get; set; }
+
+        [Required]
+        public double Tarifa { get; set; }
         public int Ocupantes { get; set;}
         public int MetrosCuadrados { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenidad { get; set; }
     }
 }
